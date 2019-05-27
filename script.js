589c98742eac7ec1f08d550491d12b9e47227c09
門田見 侑大 <yuta-kadotami@gmo.jp>
2019-05-27 13:08:44 +0900

    modify script js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 (function(){
+    var html = document.querySelector('html').outerHTML;
+    console.log(html);
+    console.log(htmlComment())
 
+})();
 
-
-});
+function htmlComment() {
+    comments = [];
+    var d = document.getElementsByTagName("html")[0].childNodes;
+    for (i=0;i<d.length;i++){
+        if(d[i].nodeType == 8){
+            comments.push(d[i].data);
+        }
+    }
+    return comments
+} 
