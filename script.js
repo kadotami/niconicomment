5ccc9058708ed1e46221103c4464bf9fe6f4d8e0
門田見 侑大 <yuta-kadotami@gmo.jp>
2019-05-27 16:10:24 +0900

    modify extract html comment

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 (function(){
+    
+    createCss()
     var html = document.querySelector('html').outerHTML;
     console.log(html);
-    console.log(htmlComment())
+    console.log(htmlComment(html))
+    appendComments(htmlComment(html));
 
 })();
 
-function htmlComment() {
-    comments = [];
-    var d = document.getElementsByTagName("html")[0].childNodes;
-    for (i=0;i<d.length;i++){
-        if(d[i].nodeType == 8){
-            comments.push(d[i].data);
-        }
-    }
-    return comments
+function htmlComment(html) {
+    var result = html.match(/<!--(.*?)-->/gi);
+    return result;
 } 
+
+function createCss() {
+    var style = document.createElement("style");
+    var css = document.createTextNode('\
+        #niconicomments li {position: fixed; top: 0; z-index: 99999;}\
+    ');
+    style.appendChild(css);
+    document.body.appendChild(style);
+}
+
+function appendComments(comments) {
+    var ul = document.createElement("ul");
+    ul.id = 'niconicomments';
+    comments.forEach(element => {
+        var li = document.createElement("li");
+        var comment = document.createTextNode(element);
+        li.appendChild(comment);
+        ul.appendChild(li);
+    });
+    document.body.appendChild(ul);
+}
